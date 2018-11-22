1532cc09a017dbb4eccdf660956de5fabd8ad3af
VR Desktop AP Hogeschool <storyeps2018@gmail.com>
2018-11-22 12:15:50 +0100

    Add Test Project
    Add Test Project with correct settings

---
+++ Test Project/Assets/LightSourceScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSourceScript : MonoBehaviour {
+
+    GameObject lightGameObject;
+    Light lightComp;
+    bool test = true;
+    // Use this for initialization
+    void Start () {
+        transform.
+        lightGameObject = new GameObject("Light");
+        lightComp = lightGameObject.AddComponent<Light>();
+        lightComp.color = Color.red;
+	}
+	
+
+
+	// Update is called once per frame
+	void Update () {
+
+
+        lightGameObject.transform.Translate(Vector3.right);
+    }
+}
