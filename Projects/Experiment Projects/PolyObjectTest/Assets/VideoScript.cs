a18aadc5cc8b29bcb647e656bd42c36e059b5092
VR Desktop AP Hogeschool <storyeps2018@gmail.com>
2018-12-07 11:25:45 +0100

    Add Poly Test project

---
+++ Projects/Experiment Projects/PolyObjectTest/Assets/VideoScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoScript : MonoBehaviour {
+
+    public VideoPlayer VideoPlayer;
+
+	// Use this for initialization
+	void Start () {
+        startTakeNumber(1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void startTakeNumber(int numberOfTake)
+    {
+        VideoPlayer.url = "Assets/Footage/" + numberOfTake.ToString() + ".MP4";
+        VideoPlayer.Play();
+    }
+}
