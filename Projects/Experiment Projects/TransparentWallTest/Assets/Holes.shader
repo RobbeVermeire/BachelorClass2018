80b60ac4b82b18bdccea4a250945d0fa5586a1e4
RobbeV <robbe.vermeire@student.ap.be>
2018-12-12 09:02:58 +0100

    Update TransparentWallTest

---
+++ Projects/Experiment Projects/TransparentWallTest/Assets/Holes.shader
@@ -0,0 +1,28 @@
+﻿Shader "Holes" {
+     Properties {
+       _MainTex ("Texture (RGB)", 2D) = "white" {}
+       [PerRendererData] _SliceGuide ("Slice Guide (RGB)", 2D) = "white" {}
+       _SliceAmount ("Slice Amount", Range(0.0, 1.0)) = 0.9
+     }
+     SubShader {
+       Tags { "RenderType" = "Opaque" }
+       Cull Off
+       CGPROGRAM
+       //if you're not planning on using shadows, remove "addshadow" for better performance
+       #pragma surface surf Lambert 
+       struct Input {
+           float2 uv_MainTex;
+           float2 uv_SliceGuide;
+           float _SliceAmount;
+       };
+       sampler2D _MainTex;
+       sampler2D _SliceGuide;
+       float _SliceAmount;
+       void surf (Input IN, inout SurfaceOutput o) {
+           clip(tex2D (_SliceGuide, IN.uv_SliceGuide).rgb - _SliceAmount);
+           o.Albedo = tex2D (_MainTex, IN.uv_MainTex).rgb;
+       }
+       ENDCG
+     }
+     Fallback "Diffuse"
+   }
--- Projects/Experiment Projects/TransparentWallTest/Assets/makeTransparent.cs
+++ Projects/Experiment Projects/TransparentWallTest/Assets/makeTransparent.cs
@@ -5,29 +5,57 @@ using UnityEngine;
 public class makeTransparent : MonoBehaviour {
 
 
+    Color color = new Color(0F,0F,0F, 1);
+    Texture2D SliceGuideTex;
+    Renderer ObjectRenderer;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
+        ObjectRenderer = this.gameObject.GetComponent<Renderer>();
+        SliceGuideTex = new Texture2D(16, 16);
 
-	}
+        // set texture in the inspector slot
+        ObjectRenderer.material.SetTexture("_SliceGuide", SliceGuideTex);
+
+        // Fill the texture with white (you could also paint it black, then draw with white)
+        for (int y = 0; y < SliceGuideTex.height; ++y) 
+        {
+            for (int x= 0; x < SliceGuideTex.width; ++x) 
+            {
+                SliceGuideTex.SetPixel(x, y, Color.white);
+            }
+        }
+        // Apply all SetPixel calls
+        SliceGuideTex.Apply();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if (!Input.GetMouseButton(0))
-            return;
 
+        if (Input.touchCount == 0 && !Input.anyKey) return;
+
+        // Only if we hit something, do we continue
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hit))
-        {
-            Renderer renderer = hit.collider.GetComponent<Renderer>();
-            MeshCollider meshCollider = hit.collider as MeshCollider;
-            Texture2D tex = (Texture2D)renderer.material.mainTexture;
 
-            Vector2 vec = new Vector2(tex.width, tex.height);
+        if (!Physics.Raycast(ray, out hit)) return;
 
-            Debug.Log(hit.textureCoord*vec);
-        }
+        // Just in case, also make sure the collider also has a renderer
+        // material and texture. Also we should ignore primitive colliders.
+        Renderer renderer = hit.collider.gameObject.GetComponent<Renderer>();
+
+        var meshCollider = hit.collider as MeshCollider;
+        if (renderer == null || renderer.sharedMaterial == null || SliceGuideTex == null || meshCollider == null) return;
+
+        // Now draw a pixel where we hit the object
+        Texture2D tex = SliceGuideTex;
+        var pixelUV = hit.textureCoord;
+        pixelUV.x *= tex.width;
+        pixelUV.y *= tex.height;
+
+        // add black spot, which is then transparent in the shader
+        tex.SetPixel((int)pixelUV.x, (int)pixelUV.y, Color.black);
+        tex.Apply();
 
     }
 }
